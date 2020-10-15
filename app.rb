fc54a13d320101fa62a62e9918c978cce196611b
Bala Luguja <bluguja@gmail.com>
2020-10-15 08:51:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+get '/hello' do 
+		erb:hello #loading hello.erb in browser
+	end
 
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+
+	get '/date' do
+		erb :date
+	end
 end
